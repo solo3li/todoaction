18b20c1ca9c103e4517e6ee202e21e41d6bee288
solo3li <hamed3alii.3@gmail.com>
2026-05-04 14:58:06 -0700

    host

--- donation_project/settings.py
+++ donation_project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'django-insecure-2c$)2x%anwoyist97=1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
